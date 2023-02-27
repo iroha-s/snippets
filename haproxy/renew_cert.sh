33b8844c02b9a1cacdf2f7734d3100b2b708a75a
iroha <git@iroha-s.jp>
2023-02-27 21:29:28 +0900

    first commit

---
+++ haproxy/renew_cert.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SITE=$1
+
+echo $1
+
+# mkdir -p /etc/haproxy/certs
+
+# # move to the correct let's encrypt directory
+# cd /etc/letsencrypt/live/$SITE
+
+# # cat files to make combined .pem for haproxy
+# cat fullchain.pem privkey.pem > /etc/haproxy/certs/$SITE.pem
+
+# # reload haproxy
+# service haproxy reload
