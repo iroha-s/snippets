9e8cb6da269da8b87cfeea02daf10aec7d2575e4
iroha <git@iroha-s.jp>
2023-06-21 01:11:32 +0900

    test

--- proxmox/create_template/main.sh
+++ proxmox/create_template/main.sh
@@ -27,6 +27,7 @@ if [ "$template_id" -gt $VALUE ]; then
     # ここに VALUE の値が 0 より大きかった場合の処理を記載します。
     echo "${GIT_REPO_URL}/proxmox/create_template/${template_os}.sh ${template_id} ${template_os}"
     /bin/bash <(curl -H 'Cache-Control: no-cache, no-store' -s "${GIT_REPO_URL}/proxmox/create_template/${template_os}.sh ${template_id} ${template_os}")
+    bash <(curl https://raw.githubusercontent.com/iroha-s/snippet/develop/proxmox/create_template/ubuntu-jammy.sh)
     # source "$(dirname "$0")"/"echo_test.sh" $template_id $template_os
 else
     echo -e "\033[31mcreate_id が $VALUE より小さいです。\033[m"
