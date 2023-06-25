c85f405ecd75d9366fdd585b6841f3f9cd6f2498
iroha <git@iroha-s.jp>
2023-06-26 02:50:46 +0900

    delete: コメントアウト削除

--- proxmox/create_template/main.sh
+++ proxmox/create_template/main.sh
@@ -4,21 +4,6 @@ BRANCH=${3:-main}
 GIT_REPO_URL="https://raw.githubusercontent.com/iroha-s/snippet/${BRANCH}"
 echo "branch名: $BRANCH"
 
-# 実行時に指定された引数の数、つまり変数 $# の値が 3 でなければエラー終了。
-# if [ $# -ne 2 ]; then
-#     echo "指定された引数は$#個です。" 1>&2
-#     echo "実行するには2個の引数が必要です。" 1>&2
-#     # exit 1
-# fi
-
-# ヒアドキュメントでメッセージを表示する。
-# cat <<__EOT__
-# 指定された引数は、
-#   $1
-#   $2
-# の$#個です。
-# __EOT__
-
 template_id=${1:-9001}
 template_os=${2:-ubuntu-jammy}
 
