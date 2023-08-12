52f08d589295198025b23896ebfbda8c25dde125
iroha <git@iroha-s.jp>
2023-08-13 08:27:39 +0900

    add: vm-deploy.sh

---
+++ kubernetes/vm-deploy.sh
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+TARGET_BRANCH=$1
+TEMPLATE_VMID=9100
+CLOUDINIT_IMAGE_TARGET_VOLUME=local-lvm
+TEMPLATE_BOOT_IMAGE_TARGET_VOLUME=local-lvm
+BOOT_IMAGE_TARGET_VOLUME=local-lvm
+SNIPPET_TARGET_VOLUME=local-lvm
+SNIPPET_TARGET_PATH=/opt/k8s-snippets/${SNIPPET_TARGET_VOLUME}/snippets
+REPOSITORY_RAW_SOURCE_URL="https://raw.githubusercontent.com/iroha-s/snippet/${TARGET_BRANCH}"
+VM_LIST=(
+    # ---
+    # vmid:       proxmox上でVMを識別するID
+    # vmname:     proxmox上でVMを識別する名称およびホスト名
+    # cpu:        VMに割り当てるコア数(vCPU)
+    # mem:        VMに割り当てるメモリ(MB)
+    # vmsrvip:    VMのService Segment側NICに割り振る固定IP
+    # vmsanip:    VMのStorage Segment側NICに割り振る固定IP
+    # targetip:   VMの配置先となるProxmoxホストのIP
+    # targethost: VMの配置先となるProxmoxホストのホスト名
+    # ---
+    #vmid #vmname #cpu #mem  #vmsrvip    #vmsanip     #targetip     #targethost
+    "301 k8s-cp-1 2   8192 192.168.1.111 172.16.17.11 192.168.0.1 pve-sv1"
+    "302 k8s-cp-2 2   8192 192.168.1.112 172.16.17.12 192.168.0.1 pve-sv1"
+    "303 k8s-cp-3 2   8192 192.168.1.113 172.16.17.13 192.168.0.1 pve-sv1"
+    "311 k8s-wk-1 4   8192 192.168.1.121 172.16.17.21 192.168.0.1 pve-sv1"
+    "312 k8s-wk-2 4   8192 192.168.1.122 172.16.17.22 192.168.0.1 pve-sv1"
+    "313 k8s-wk-3 4   8192 192.168.1.123 172.16.17.23 192.168.0.1 pve-sv1"
+)
+
+
+# region : create template-vm
+
+# download the image(ubuntu 22.04 LTS)
+wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+
+# create a new VM and attach Network Adaptor
+qm create $TEMPLATE_VMID --cores 2 --memory 2048 --net0 virtio,bridge=vmbr0  --name k8s-template
+
+# import the downloaded disk to $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME storage
+qm importdisk $TEMPLATE_VMID jammy-server-cloudimg-amd64.img $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME
+
+# finally attach the new disk to the VM as scsi drive
+qm set $TEMPLATE_VMID --scsihw virtio-scsi-pci --scsi0 $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME:vm-$TEMPLATE_VMID-disk-0
+
+# add Cloud-Init CD-ROM drive
+qm set $TEMPLATE_VMID --ide2 $CLOUDINIT_IMAGE_TARGET_VOLUME:cloudinit
+
+# set the bootdisk parameter to scsi0
+qm set $TEMPLATE_VMID --boot c --bootdisk scsi0
+
+# set serial console
+qm set $TEMPLATE_VMID --serial0 socket --vga serial0
+
+# migrate to template
+qm template $TEMPLATE_VMID
+
+# cleanup
+rm jammy-server-cloudimg-amd64.img
+
+for array in "${VM_LIST[@]}"
+do
+    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip vmsanip targetip targethost
+    do
+        # clone from template
+        # in clone phase, can't create vm-disk to local volume
+        qm clone "${TEMPLATE_VMID}" "${vmid}" --name "${vmname}" --full true --target "${targethost}"
+        
+        # set compute resources
+        ssh -n "${targetip}" qm set "${vmid}" --cores "${cpu}" --memory "${mem}"
+
+        # move vm-disk to local
+        ssh -n "${targetip}" qm move-disk "${vmid}" scsi0 "${BOOT_IMAGE_TARGET_VOLUME}" --delete true
+
+        # resize disk (Resize after cloning, because it takes time to clone a large disk)
+        ssh -n "${targetip}" qm resize "${vmid}" scsi0 30G
+
+        # create snippet for cloud-init(user-config)
+        # START irregular indent because heredoc
+# ----- #
+cat > "$SNIPPET_TARGET_PATH"/"$vmname"-user.yaml << EOF
+#cloud-config
+hostname: ${vmname}
+timezone: Asia/Tokyo
+manage_etc_hosts: true
+chpasswd:
+  expire: False
+users:
+  - default
+  - name: cloudinit
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    lock_passwd: false
+    # mkpasswd --method=SHA-512 --rounds=4096
+    # password is zaq12wsx
+    passwd: \$6\$rounds=4096\$Xlyxul70asLm\$9tKm.0po4ZE7vgqc.grptZzUU9906z/.vjwcqz/WYVtTwc5i2DWfjVpXb8HBtoVfvSY61rvrs/iwHxREKl3f20
+ssh_pwauth: true
+ssh_authorized_keys: []
+package_upgrade: true
+runcmd:
+  # set ssh_authorized_keys
+  - su - cloudinit -c "mkdir -p ~/.ssh && chmod 700 ~/.ssh"
+  - su - cloudinit -c "curl -sS https://github.com/iroha-s.keys >> ~/.ssh/authorized_keys"
+  - su - cloudinit -c "chmod 600 ~/.ssh/authorized_keys"
+  # run install scripts
+  - su - cloudinit -c "curl -s ${REPOSITORY_RAW_SOURCE_URL}/scripts/k8s-node-setup.sh > ~/k8s-node-setup.sh"
+  - su - cloudinit -c "sudo bash ~/k8s-node-setup.sh ${vmname} ${TARGET_BRANCH}"
+  # change default shell to bash
+  - chsh -s $(which bash) cloudinit
+EOF
+# ----- #
+        # END irregular indent because heredoc
+
+        # create snippet for cloud-init(network-config)
+        # START irregular indent because heredoc
+# ----- #
+cat > "$SNIPPET_TARGET_PATH"/"$vmname"-network.yaml << EOF
+version: 1
+config:
+  - type: physical
+    name: ens18
+    subnets:
+    - type: static
+      address: '${vmsrvip}'
+      netmask: '255.255.252.0'
+      gateway: '192.168.3.254'
+  - type: nameserver
+    address:
+    - '192.168.3.254'
+    search:
+    - 'local'
+EOF
+# ----- #
+        # END irregular indent because heredoc
+
+        # set snippet to vm
+        ssh -n "${targetip}" qm set "${vmid}" --cicustom "user=${SNIPPET_TARGET_VOLUME}:snippets/${vmname}-user.yaml,network=${SNIPPET_TARGET_VOLUME}:snippets/${vmname}-network.yaml"
+
+    done
+done
+
+for array in "${VM_LIST[@]}"
+do
+    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip vmsanip targetip targethost
+    do
+        # start vm
+        ssh -n "${targetip}" qm start "${vmid}"
+        
+    done
+done
