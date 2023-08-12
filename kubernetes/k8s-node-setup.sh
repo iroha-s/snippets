c75e5289b6ef714a69ee15297e4675b8be79e3ae
iroha <76248852+iroha-s@users.noreply.github.com>
2023-08-13 02:45:47 +0900

    Update k8s-node-setup.sh

--- kubernetes/k8s-node-setup.sh
+++ kubernetes/k8s-node-setup.sh
@@ -36,6 +36,8 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 # Kubernetesの aptリポジトリを追加
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+sleep 5
+
 # kubeadm,kubelet,kubectlインストール
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
