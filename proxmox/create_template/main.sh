5fc93a891eb4f8efe2288b2bb37a24f45d4e1a3b
iroha <git@iroha-s.jp>
2023-09-13 05:56:35 +0900

    update

--- proxmox/create_template/main.sh
+++ proxmox/create_template/main.sh
@@ -10,7 +10,7 @@ storage=${3:-local-lvm}
 VALUE=9000
 if [ "$template_id" -gt $VALUE ]; then
     # ここに VALUE の値が 0 より大きかった場合の処理を記載します。
-    echo "${GIT_REPO_URL}/proxmox/create_template/${template_os}.sh ${template_id} ${template_os}"
+    echo "${GIT_REPO_URL}/proxmox/create_template/${template_os}.sh ${template_id} ${template_os} ${storage}"
     /bin/bash <(curl -H 'Cache-Control: no-cache, no-store' -s "${GIT_REPO_URL}/proxmox/create_template/${template_os}.sh") ${template_id} ${template_os} ${storage}
     # /bin/bash <(curl -s https://raw.githubusercontent.com/iroha-s/snippet/develop/proxmox/create_template/ubuntu-jammy.sh)
     # source "$(dirname "$0")"/"echo_test.sh" $template_id $template_os
